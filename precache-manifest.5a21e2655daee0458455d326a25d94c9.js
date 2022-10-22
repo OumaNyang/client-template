5891dac6a2678688ecd27702aa6d2c819f01ded1
Milton Ouma <ouma.nyang1@gmail.com>
2022-10-22 16:26:12 +0300

    Updates

---
+++ precache-manifest.5a21e2655daee0458455d326a25d94c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73fd77228ed3a0d49390feb44b89f53d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aaec4b7868d920fcd4d8",
+    "url": "/static/css/2.a779dd76.chunk.css"
+  },
+  {
+    "revision": "1f0ed8c6ebf879a0167b",
+    "url": "/static/css/main.070ea7ba.chunk.css"
+  },
+  {
+    "revision": "aaec4b7868d920fcd4d8",
+    "url": "/static/js/2.edfbd759.chunk.js"
+  },
+  {
+    "revision": "caa90a56c02f1c374365bab2e8bfbcc4",
+    "url": "/static/js/2.edfbd759.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f0ed8c6ebf879a0167b",
+    "url": "/static/js/main.bc776a3c.chunk.js"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
